24ee426687c07be12a3504c6494b3525e641f724
Kutay Zeki <kutayzeki@gmail.com>
2023-01-09 20:10:21 +0300

    response viewmodels

---
+++ BackendTemplate/ViewModels/ResponseModels/ApiResponseViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace BackendTemplate.ViewModels.ResponseModels
+{
+    public class ApiResponseViewModel
+    {
+        public int Id { get; set; }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+
+}
+
+/* 
+ 
+public IActionResult PostProduct(Product product)
+{
+    // Save the product to the database
+    _context.Products.Add(product);
+    _context.SaveChanges();
+
+    // Create the view model
+    var model = new ApiResponseViewModel
+    {
+        Id = product.Id,
+        IsSuccess = true,
+        Message = "Product created successfully"
+    };
+
+    // Return the view model in the response
+    return Ok(model);
+}
+
+ */
---
+++ BackendTemplate/ViewModels/ResponseModels/PagedApiResponseViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace BackendTemplate.ViewModels.ResponseModels
+{
+    public class PagedApiResponseViewModel<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; }
+        public IEnumerable<T> Data { get; set; }
+    }
+}
+
+/*
+ * public IActionResult GetProducts(int pageNumber, int pageSize)
+{
+    // Retrieve the total number of products
+    var totalRecords = _context.Products.Count();
+
+    // Calculate the total number of pages
+    var totalPages = (int)Math.Ceiling((double)totalRecords / pageSize);
+
+    // Retrieve the paginated products
+    var products = _context.Products
+        .Skip((pageNumber - 1) * pageSize)
+        .Take(pageSize)
+        .ToList();
+
+    // Create the view model
+    var model = new PagedApiResponseViewModel<Product>
+    {
+        PageNumber = pageNumber,
+        PageSize = pageSize,
+        TotalPages = totalPages,
+        TotalRecords = totalRecords,
+        Data = products
+    };
+
+    // Return the view model in the response
+    return Ok(model);
+}
+}*/
