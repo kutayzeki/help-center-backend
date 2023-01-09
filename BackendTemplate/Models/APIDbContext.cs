544e45bb729194e73524012fd8bec8f387786b91
Kutay Zeki <kutayzeki@gmail.com>
2023-01-09 23:54:56 +0300

    API DB Context

---
+++ BackendTemplate/Models/APIDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BackendTemplate.Models.User;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BackendTemplate.Models
+{
+    public class APIDbContext : IdentityDbContext<AppUser>
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> dbContext) : base(dbContext) { }
+    }
+}
