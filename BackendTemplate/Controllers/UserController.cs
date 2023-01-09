dadcf8fc18c1753f8837fda6008c56691a130340
Kutay Zeki <kutayzeki@gmail.com>
2023-01-09 23:55:39 +0300

    versioning

---
+++ BackendTemplate/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using BackendTemplate.Models;
+using BackendTemplate.Models.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendTemplate.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    public class UserController : ControllerBase
+    {
+        private readonly APIDbContext _context;
+        private readonly IConfiguration _config;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserController(APIDbContext context,
+                              IConfiguration configuration,
+                              SignInManager<AppUser> signInManager,
+                              UserManager<AppUser> userManager,
+                              RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _config = configuration;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [MapToApiVersion("1.0")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUserData(int id)
+        {
+            // Check if the user exists
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+
+            return NoContent();
+        }
+    }
+}
--- BackendTemplate/Program.cs
+++ BackendTemplate/Program.cs
@@ -1,23 +1,79 @@
+using BackendTemplate;
+using BackendTemplate.Models;
+using BackendTemplate.Models.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+// or using the index property (which always returns a string)
+var connectionString = builder.Configuration.GetConnectionString("Connection");
+
+builder.Services.AddDbContext<APIDbContext>(options =>
+options.UseSqlServer(connectionString), ServiceLifetime.Transient);
+
+builder.Services
+    .AddIdentity<AppUser, IdentityRole>()
+    .AddRoleManager<RoleManager<IdentityRole>>()
+    .AddEntityFrameworkStores<APIDbContext>().AddDefaultTokenProviders();
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+
+builder.Services.AddApiVersioning(opt =>
+{
+    opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    opt.AssumeDefaultVersionWhenUnspecified = true;
+    opt.ReportApiVersions = true;
+    opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                    new HeaderApiVersionReader("x-api-version"),
+                                                    new MediaTypeApiVersionReader("x-api-version"));
+});
+// ApiExplorer to discover versions
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+    setup.GroupNameFormat = "'v'VVV";
+    setup.SubstituteApiVersionInUrl = true;
+});
+
 var app = builder.Build();
+
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                description.GroupName.ToUpperInvariant());
+        }
+    });
 }
+
+/*app.UseRequestLocalization(options);
+app.UseStaticFiles();
+app.UseMiddleware<LocalizerMiddleware>();*/
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
