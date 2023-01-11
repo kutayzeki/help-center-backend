39184ac23442622d3d345bb4059a9bffbe086d82
Kutay Zeki <kutayzeki@gmail.com>
2023-01-11 20:38:32 +0300

    Notifications

---
+++ BackendTemplate/Controllers/NotificationController.cs
@@ -0,0 +1,27 @@
+﻿using BackendTemplate.Core.Services.NotificationService;
+using BackendTemplate.Models.Notification;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendTemplate.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [Route("send")]
+        [HttpPost]
+        public async Task<IActionResult> SendNotification(Notification notificationModel)
+        {
+            var result = await _notificationService.SendNotification(notificationModel);
+            return Ok(result);
+        }
+    }
+}
---
+++ BackendTemplate/Models/Notification/FcmNotificationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BackendTemplate.Models.Notification
+{
+    public class FcmNotificationSetting
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public string SenderId { get; set; }
+        public string ServerKey { get; set; }
+    }
+}
